352efd10539159711ebcf4fb64a3a9c87ea5963d
CamilaFre <camila.frech@hotmail.com>
2024-10-13 21:17:26 -0300

    adicionar o ex. de data

--- exercicios.py
+++ exercicios.py
@@ -5,6 +5,16 @@
 
 import math
 
-raio_do_circulo = float(input('Digite o raio: '))
-area_do_circulo = math.pi*raio_do_circulo**2
-print(f"{area_do_circulo:.2F}")
+#raio_do_circulo = float(input('Digite o raio: '))
+#area_do_circulo = math.pi*raio_do_circulo**2
+#print(f"{area_do_circulo:.2F}")
+
+
+data_do_usuario= input("Insira uma data no formato dd/mm/aaaa: ")
+data_separada = data_do_usuario.split("/")
+##print(data_separada)
+dia = print(data_separada[0])
+dia = print(data_separada[1])
+dia = print(data_separada[2])
+
+
