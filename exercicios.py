fcb769fb5c2c8f68c0ce572dd362819832793685
CamilaFre <camila.frech@hotmail.com>
2024-10-13 21:23:21 -0300

    adicionar type execpt

--- exercicios.py
+++ exercicios.py
@@ -1,20 +1,23 @@
-#n1 = float(input("Digite o número 1: "))
-#n2 = float(input("Digite o número 2: "))
-#divisao =  n1//n2
-#print(divisao)
+try:
+    n1 = float(input("Digite o número 1: "))
+    n2 = float(input("Digite o número 2: "))
+    divisao =  n1//n2
+    print(divisao)
+except:
+    print("não existe divisão por zero")
 
-import math
+#import math
 
 #raio_do_circulo = float(input('Digite o raio: '))
 #area_do_circulo = math.pi*raio_do_circulo**2
 #print(f"{area_do_circulo:.2F}")
 
 
-data_do_usuario= input("Insira uma data no formato dd/mm/aaaa: ")
-data_separada = data_do_usuario.split("/")
+#data_do_usuario= input("Insira uma data no formato dd/mm/aaaa: ")
+#data_separada = data_do_usuario.split("/")
 ##print(data_separada)
-dia = print(data_separada[0])
-dia = print(data_separada[1])
-dia = print(data_separada[2])
+#dia = print(data_separada[0])
+#dia = print(data_separada[1])
+#dia = print(data_separada[2])
 
 
