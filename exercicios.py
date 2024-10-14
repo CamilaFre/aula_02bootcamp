85956bc97cfd9708412e67a244ad292cdcb5152b
CamilaFre <camila.frech@hotmail.com>
2024-10-13 21:10:00 -0300

    adicionar exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,10 @@
-n1 = float(input("Digite o número 1: "))
-n2 = float(input("Digite o número 2: "))
-divisao =  n1//n2
-print(divisao)
+#n1 = float(input("Digite o número 1: "))
+#n2 = float(input("Digite o número 2: "))
+#divisao =  n1//n2
+#print(divisao)
+
+import math
+
+raio_do_circulo = float(input('Digite o raio: '))
+area_do_circulo = math.pi*raio_do_circulo**2
+print(f"{area_do_circulo:.2F}")
